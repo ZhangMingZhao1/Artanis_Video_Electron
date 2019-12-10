6edbf40b8f61da2a7c800b9e32608d41463a85f1
ZhangMingZhao1 <1104272319@qq.com>
2019-12-10 23:48:42 +0800

    fix:渲染进程通信主进程&任务列表

--- reactView/src/component/huyaItem/index.tsx
+++ reactView/src/component/huyaItem/index.tsx
@@ -1,53 +1,94 @@
-import React , {useState,useEffect} from "react";
-// import { ipcRenderer } from "electron";
+import React, { useState, useEffect } from "react";
 const ipcRenderer = window.electron.ipcRenderer;
 import { Button, Input } from "rsuite";
-import {Table} from 'antd';
+import { Table } from "antd";
 import "./index.less";
 interface Props {
-  data:{
+  data: {
     // onClick: any;
     platformName: string;
-    platformKey: string
-  }
+    platformKey: string;
+  };
 }
 
 function Item(props: Props) {
-  const [huyaUrl,setHuyaUrl] = useState("");
-  const [pidArr,setPidArr] = useState<Array<string>>([]);
-  useEffect(()=>{
-    ipcRenderer.on('processInfo',(pid:any)=>{
-      const nextPidArr = [...pidArr,pid];
-      setPidArr(nextPidArr);
-    })
-  },[])
-  // ipcRenderer.on()
+  const [huyaUrl, setHuyaUrl] = useState("");
+  const [dataArr, setDataArr] = useState<Array<{ pid: string; name: string }>>(
+    []
+  );
+  useEffect(() => {
+    ipcRenderer.on(
+      "processInfo",
+      (streamInfo: { pid: string; name: string }) => {
+        const nextDataArr = [...dataArr, streamInfo];
+        setDataArr(nextDataArr);
+      }
+    );
+    return () => {
+      ipcRenderer.removeListener("processInfo");
+    };
+  }, []);
   function handleClick() {
     ipcRenderer.send(`async${props.data.platformKey}-message`, huyaUrl);
-    console.log('cc')
+    console.log("cc");
   }
   function handleChange(value: string) {
     console.log(value);
     setHuyaUrl(value);
   }
   function stopDown() {
-    ipcRenderer.send(`async${props.data.platformKey}-message`, 'stop');
+    ipcRenderer.send(`async${props.data.platformKey}-message`, "stop");
     // console.log('cc')
   }
+  const columns = [
+    {
+      title: "进程号",
+      dataIndex: "pid",
+      key: "pid"
+    },
+    {
+      title: "信息",
+      dataIndex: "name",
+      key: "name"
+    }
+  ];
   return (
     <>
-    <div className="item">
-      <div style={{marginLeft:30,marginRight:30, fontSize:18, fontWeight:400, color:"#1cbbb4"}}>请输入{props.data.platformName}房间号：</div>
-      <Input onChange={handleChange} style={{ width: 300,marginRight:30 }} size="md" />
-      <Button style={{marginRight:20}} appearance="primary" onClick={handleClick}>开始下载保存视频</Button>
-      <Button style={{color:"red"}} appearance="default" onClick={stopDown}>停止下载</Button>
-    </div>
-    <div className="list">
-    <Table
-      columns={}
-      // dataSource={}
-      />
-    </div>
+      <div className="item">
+        <div
+          style={{
+            marginLeft: 30,
+            marginRight: 30,
+            fontSize: 18,
+            fontWeight: 400,
+            color: "#1cbbb4"
+          }}
+        >
+          请输入{props.data.platformName}房间号：
+        </div>
+        <Input
+          onChange={handleChange}
+          style={{ width: 300, marginRight: 30 }}
+          size="md"
+        />
+        <Button
+          style={{ marginRight: 20 }}
+          appearance="primary"
+          onClick={handleClick}
+        >
+          开始下载保存视频
+        </Button>
+        <Button
+          style={{ color: "red" }}
+          appearance="default"
+          onClick={stopDown}
+        >
+          停止下载
+        </Button>
+      </div>
+      <div className="list">
+        <Table columns={columns} dataSource={dataArr} />
+      </div>
     </>
   );
 }
--- src/engine/huya/message.ts
+++ src/engine/huya/message.ts
@@ -1,64 +1,71 @@
-const log4js = require('log4js');
-const dayjs = require('dayjs');
+const log4js = require("log4js");
+const dayjs = require("dayjs");
 const rootPath = process.cwd();
-const {ipcMain,ipcRenderer} = require('electron');
-const { spawn } = require('child_process');
-import getHuya from './getHuyaStreamUrl';
+const { ipcMain, ipcRenderer } = require("electron");
+const { spawn } = require("child_process");
+import getHuya from "./getHuyaStreamUrl";
 log4js.configure({
   appenders: {
-    cheese:{
-      type: 'file',
-      filename: rootPath+ '/logs/artanis.log',
-      maxLogSize : 20971520,
-      backups : 10,
-      encoding : 'utf-8',
-    },
+    cheese: {
+      type: "file",
+      filename: rootPath + "/logs/artanis.log",
+      maxLogSize: 20971520,
+      backups: 10,
+      encoding: "utf-8"
+    }
   },
-  categories: { default: { appenders: ['cheese'], level: 'info' } }
-})
+  categories: { default: { appenders: ["cheese"], level: "info" } }
+});
 
-const logger = log4js.getLogger('message');
+const logger = log4js.getLogger("message");
 
-export default ()=> {
+export default () => {
   // 虎牙消息监听
-  const pool:any = [];
-  ipcMain.on('asynchuya-message', (event, huyaRoomId) => {
-    console.log('huyaRoomId: ',huyaRoomId) // prints "ping"
-    if(huyaRoomId !== 'stop') {
-      getHuya(huyaRoomId).then((streamUrl=>{
-        // const cmd = `ffmpeg -i "${streamUrl}" -f mp4 res.MP4`;
-        // 命名加上时间戳
-        const timeV = dayjs().format('YYYY-MM-DD');
-        const cmd = `ffmpeg`;
-        const huyaApp = spawn(cmd,['-i',streamUrl,'-f','mp4',huyaRoomId+`${timeV}res.MP4`]);
-        // console.log('11111',huyaApp,JSON.stringify(huyaApp));
-        // 发送
-        ipcRenderer.send('processInfo', huyaApp.pid)
-        
-        pool.push(huyaApp);
-        huyaApp.stdout.on('data', (data:any) => {
-          // console.log(`stdout: ${data}`);
-          logger.info(data.toString("utf8"));
-        });
-        huyaApp.stderr.on('data', (data:any) => {
-          // console.error(`stderr: ${data}`);
-          logger.info(data.toString("utf8"));
-        });
-        huyaApp.on('close', (code:any) => {
-          // console.log(`子进程退出，退出码 ${code}`);
-          logger.info(`子进程退出，退出码 ${code}`);
+  const pool: any = [];
+  ipcMain.on("asynchuya-message", (event, huyaRoomId) => {
+    console.log("huyaRoomId: ", huyaRoomId); // prints "ping"
+    if (huyaRoomId !== "stop") {
+      getHuya(huyaRoomId).then(
+        (stream: { streamUrl: string; streamName: string }) => {
+          // const cmd = `ffmpeg -i "${streamUrl}" -f mp4 res.MP4`;
+          // 命名加上时间戳
+          const timeV = dayjs().format("YYYY-MM-DD");
+          const cmd = `ffmpeg`;
+          const huyaApp = spawn(cmd, [
+            "-i",
+            stream.streamUrl,
+            "-f",
+            "mp4",
+            huyaRoomId + `${timeV}res.MP4`
+          ]);
+          // 发送
+          event.sender.send("processInfo", {
+            pid: huyaApp.pid,
+            name: stream.streamName
+          });
 
-        });
-  
-      }))
-    }else {
-      if(pool.length) {
-        pool[0].kill()
+          pool.push(huyaApp);
+          huyaApp.stdout.on("data", (data: any) => {
+            // console.log(`stdout: ${data}`);
+            logger.info(data.toString("utf8"));
+          });
+          huyaApp.stderr.on("data", (data: any) => {
+            // console.error(`stderr: ${data}`);
+            logger.info(data.toString("utf8"));
+          });
+          huyaApp.on("close", (code: any) => {
+            // console.log(`子进程退出，退出码 ${code}`);
+            logger.info(`子进程退出，退出码 ${code}`);
+          });
+        }
+      );
+    } else {
+      if (pool.length) {
+        pool[0].kill();
         logger.error(`手动关闭了下载`);
       }
     }
-
-  })
+  });
 
   // ipcMain.on('asynchuya-stop-message', (event,message)=> {
   //   console.log(message);
@@ -73,7 +80,4 @@ export default ()=> {
   //   //   })
   //   // }
   // })
-}
-
-
-
+};
